0e4cbee5001c240deab40469dd40f42b650538ca
SamillyNunes <samillynunes19@gmail.com>
2021-05-21 11:39:34 -0300

    Working with POO: Building Pessoa class

---
+++ src/main/kotlin/Pessoa.kt
@@ -0,0 +1,13 @@
+// Classe de apenas objetos valor (sem codigo, só dados)
+// Por baixo dos panos, o kotlin gera um bytecode para essa classe em java
+class Pessoa (
+    val nome:String,
+    val idade:Int
+)
+
+fun main(){
+    val pessoa1:Pessoa = Pessoa("Sam",21)
+    val pessoa2:Pessoa = Pessoa("Leo",23)
+    println("Pessoa 1: ${pessoa1.nome}")
+    println("Pessoa 2: ${pessoa2.nome}")
+}
