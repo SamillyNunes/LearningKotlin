120d6a378c473840d95442517d8957ae37ab975f
SamillyNunes <samillynunes19@gmail.com>
2021-05-21 16:05:03 -0300

    Working with packages

---
+++ src/main/kotlin/br/com/samillynunes/pacotea/PacoteA.kt
@@ -0,0 +1,5 @@
+// deve ser unico
+// eh recomendavel, mas nao obrigatorio, que siga a estrutura de diretorios
+package br.com.samillynunes.pacotea
+
+fun nome() = "Pacote A"
---
+++ src/main/kotlin/br/com/samillynunes/pacoteb/PacoteB.kt
@@ -0,0 +1,9 @@
+package br.com.samillynunes.pacoteb
+
+// para que possamos utilizar o que tem no pacote, importamos
+import br.com.samillynunes.pacotea.nome
+
+fun main(){
+    println(nome())
+}
+
