b57e0dfad89c2771ab42805c7fe4fbd100f03f38
SamillyNunes <samillynunes19@gmail.com>
2021-05-24 18:23:26 -0300

    Working with ramifications

--- src/main/kotlin/SmartCast.kt
+++ src/main/kotlin/SmartCast.kt
@@ -7,19 +7,15 @@ class Numero(val valor: Int) : Expressao
 
 class Soma(val esquerdo:Expressao, val direito:Expressao): Expressao
 
-fun avaliacao(expressao: Expressao): Int{
-    if(expressao is Numero){
-        // esse cast abaixo nao eh necessario, pq no if ele ja validou e fez o cast
-        //val numero = expressao as Numero
-        return expressao.valor
+fun avaliacao(expressao: Expressao): Int =
+    when(expressao){
+        is Numero -> {
+            println("Expressão com número: ${expressao.valor}")
+            expressao.valor
+        }
+        is Soma -> avaliacao((expressao.esquerdo))+avaliacao(expressao.direito)
+        else -> throw IllegalArgumentException("Expressão é inválida.")
     }
-
-    if(expressao is Soma){
-        return avaliacao(expressao.esquerdo)+avaliacao(expressao.direito)
-    }
-
-    throw IllegalArgumentException("Expressão é inválida.")
-}
 
 fun main(){
     val resultado = avaliacao(Soma(Soma(Numero(1),Numero(2)),Numero(4)))
