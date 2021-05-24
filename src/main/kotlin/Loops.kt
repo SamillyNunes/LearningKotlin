91692a92d7258b4cbec11f3aecc8e9baf12646cd
SamillyNunes <samillynunes19@gmail.com>
2021-05-24 18:37:36 -0300

    Working with do-while

--- src/main/kotlin/Loops.kt
+++ src/main/kotlin/Loops.kt
@@ -1,10 +1,11 @@
 // Um programa para calcular e exibir o resultado da média de temperaturas informadas pelo usuário
 fun main(){
-    var temperatura: Int = 0
+    var temperatura: Int // podemos nao inicializar a variavel
     var somatorio: Int = 0
     var quantidade:Int =0
 
-    while (temperatura!=999){
+    // o do-while executa pelo menos uma vez
+    do {
         println("Digite uma temperatura ou 999 para sair:")
         temperatura = readLine()!!.toInt() // aqui (!!) estamos afirmando que com certeza sera digitado um numero
 
@@ -12,8 +13,12 @@ fun main(){
             somatorio+=temperatura
             quantidade++
         }
+    } while (temperatura!=999)
+
+    var media:Int = 0
+    if(quantidade!=0){
+        media = somatorio/quantidade
     }
 
-    val media = somatorio/quantidade
     println("Média: ${media}")
 }
